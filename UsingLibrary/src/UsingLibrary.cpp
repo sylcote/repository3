892d1ffe6dc13afa5ead7fa4b37c246677706217
sylcote <sylcote2107@gmail.com>
2021-08-03 21:56:26 -0400

    First commit

---
+++ UsingLibrary/src/UsingLibrary.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : UsingLibrary.cpp
+// Author      : Sylvain Côté
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "TestLibrary.h"
+
+using namespace std;
+using namespace TestLib;
+
+int main() {
+	TestLibrary myLib;
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	myLib.setx(5);
+
+	cout << "TestLibray ouput: " << myLib.toString() << " Number: " << myLib.getx() << endl;
+
+	return 0;
+}
